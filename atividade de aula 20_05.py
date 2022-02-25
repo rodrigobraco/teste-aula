80d4b8ba59c9e15ac9a3ac6d232f1f4ac9ea13fb
rodrigobraco <100388382+rodrigobraco@users.noreply.github.com>
2022-02-24 21:10:25 -0300

    Add files via upload

---
+++ atividade de aula 20_05.py
@@ -0,0 +1,18 @@
+def cria_matriz(m, n, valor):
+    matriz = []
+    for i in range(m):
+        linha = [valor] * n
+        matriz.append(linha)
+    return matriz
+def imprime_matriz(a):
+    lin_a = len(a)
+    col_a = len(a[0])
+    for i in range(0, col_a):
+        for j in range(0, col_a):
+            print('%7.2f'% a[i][j],end'')
+        print()
+        
+
+def soma_matriz(a,b):
+    lin_a = len(a)
+    col_a = len(a)
